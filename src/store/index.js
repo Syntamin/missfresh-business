5ea84bc295b7d7b77ebca85658d1bc3b01ac5dd6
Syntamin <syntamin@163.com>
2021-01-31 16:29:48 +0800

    优化初始代码结构

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 用于控制左侧菜单栏是否展开的变量， true 展开， false 收缩
+    collapsed: false,
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    changeCollapsed(context) {
+      context.commit('changeCollapsed');
+    },
   },
   modules: {
   },
---
+++ src/views/layout/components/headerNav.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <a-button
+      type="primary"
+      style="margin-bottom: 16px"
+      @click="toggleCollapsed"
+    >
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <div class="breadcrumb">
+      <a-breadcrumb>
+        <a-breadcrumb-item>首页</a-breadcrumb-item>
+        <a-breadcrumb-item><a href="">统计</a></a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <ul class="header-info">
+      <li>
+        white-also
+        <a-icon type="down" />
+      </li>
+      <li>退出</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
